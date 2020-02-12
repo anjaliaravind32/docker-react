40c78ee4c48d5b2910e8b3f61a960bb7836ea595
Anjali Aravind <anjali.aravind@gmail.com>
2020-02-12 11:34:08 -0600

    Initial push

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+		<h1 className="App-title">Welcome to React</h1>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
@@ -19,6 +20,8 @@ function App() {
           Learn React
         </a>
       </header>
+	  <p className="App-intro">
+	   To get started, edit <code>src/App.js</code></p>
     </div>
   );
 }
